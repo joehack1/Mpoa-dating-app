0c5bded07f70c6a0111ddbc55f5faf360d279b0f
joehack1 <joelluke100@gmail.com>
2026-01-15 12:56:26 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>💖 Dating App Frontend</h1>
+      <p>Vite + React setup is working.</p>
+    </div>
+  )
+}
+
+export default App
